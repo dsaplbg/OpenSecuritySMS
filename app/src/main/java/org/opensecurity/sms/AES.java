071d4164da685cb571254d87e3cc4f9b3b553124
Colas BROUX <colas.broux@free.fr>
2015-09-14 22:22:47 +0200

    Added the project
    All the necessary files to begin the project

---
+++ app/src/main/java/org/opensecurity/sms/AES.java
@@ -0,0 +1,37 @@
+package org.opensecurity.sms;
+
+import android.util.Base64;
+
+import java.security.Key;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AES {
+    private static final String ALGORITHM = "AES";
+    private static final byte[] keyValue = "TheBestSecretKey".getBytes();
+
+    public static String encrypt(String Data) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGORITHM);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = c.doFinal(Data.getBytes());
+        String encryptedValue = Base64.encodeToString(encVal, Base64.DEFAULT);
+        return encryptedValue;
+    }
+
+    public static String decrypt(String encryptedData) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGORITHM);
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decordedValue = Base64.decode(encryptedData, Base64.DEFAULT);
+        byte[] decValue = c.doFinal(decordedValue);
+        String decryptedValue = new String(decValue);
+        return decryptedValue;
+    }
+
+    private static Key generateKey() throws Exception {
+        Key key = new SecretKeySpec(keyValue, ALGORITHM);
+        return key;
+    }
+}
