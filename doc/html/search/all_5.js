283afec34c93bb471a14f6e5d5e20b83be681d46
CallisteHanriat <couim38@gmail.com>
2016-01-17 23:23:04 +0100

    mise à jour doxyfile et documentation

--- doc/html/search/all_5.js
+++ doc/html/search/all_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['findcontactbyphonenumber',['findContactByPhoneNumber',['../a00009.html#a0074262a1e69f8e9deb0ea44e55c021b',1,'org::opensecurity::sms::model::DAO']]]
+  ['findcontactbyphonenumberindefaultbase',['findContactByPhoneNumberInDefaultBase',['../a00009.html#ac877f70a0e827e3855b81178f5746b3a',1,'org::opensecurity::sms::model::DAO']]],
+  ['findcontactbyphonenumberinosmsbase',['findContactByPhoneNumberInOSMSBase',['../a00009.html#a5eb135eeb76cb1557170dc43443b0c5d',1,'org::opensecurity::sms::model::DAO']]]
 ];
